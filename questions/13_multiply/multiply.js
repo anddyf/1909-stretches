79b571f6952f6eb9f2074f57e6f6f7536729e40a
anddyf <ferreiraanddy@gmail.com>
2019-11-04 18:41:38 -0500

    stuff

--- questions/13_multiply/multiply.js
+++ questions/13_multiply/multiply.js
@@ -1,5 +1,8 @@
 const multiply = (num1, num2) => {
+    if(typeof num1 !== 'number' || typeof num1 !== 'number' ){
+        throw ''
+    }
     return num1 * num2
-};
+}
 
 module.exports = { multiply };
--- questions/13_multiply/multiply.test.js
+++ questions/13_multiply/multiply.test.js
@@ -12,18 +12,18 @@ describe('multiply testing', () => {
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    multiply(4, 5)
-    expect(multiply)
-    expect(4*5).toBe(20);
-    multiply(6, 5)
+
+    expect(multiply(4, 5)).toBe(20);
+    
     // test that the returned value is correct
-    expect(6*5).toBe(30)
+    expect(multiply(6, 5)).toBe(30)
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(3, 5)).toBe(15);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
     // test some random values
+    expect(multiply(Math.floor(Math.random()), Math.floor(Math.random()))).toBe(15);
   });
 });
